2d6f221422ac4097e4f7b306384687f4d525e6e9
manuhostin <hostimmanu@gmail.com>
2026-04-18 15:25:28 -0300

    feat

--- sitejumper/settings.py
+++ sitejumper/settings.py
@@ -137,7 +137,7 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://jogosdosalunosjumper.vercel.app/',
+    'https://jogosdosalunosjumper.vercel.app',
 
 ])
 CORS_ALLOWED_ORIGIN_REGEXES = [
